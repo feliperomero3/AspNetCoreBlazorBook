377de32198a3f28a3d105df32f56b5307f9ca9f3
Felipe Romero <feliperomero3@outlook.com>
2023-06-28 22:29:25 -0600

    Add the Blazing Trails home page.

---
+++ src/BlazingTrails.Client/Models/RouteInstruction.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazingTrails.Client.Models;
+
+/// <summary>
+/// Represent a way-point on the route to help guide other walkers.
+/// </summary>
+public class RouteInstruction
+{
+    public int Stage { get; set; }
+
+    public string Description { get; set; } = string.Empty;
+}
---
+++ src/BlazingTrails.Client/Models/Trail.cs
@@ -0,0 +1,31 @@
+﻿namespace BlazingTrails.Client.Models;
+
+/// <summary>
+/// The definition for a Trail.
+/// </summary>
+public class Trail
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public string Location { get; set; } = string.Empty;
+
+    public string Image { get; set; } = string.Empty;
+
+    public int Length { get; set; }
+
+    public int TimeInMinutes { get; set; }
+
+    public TimeSpan Duration
+    {
+        get
+        {
+            return TimeSpan.FromMinutes(TimeInMinutes);
+        }
+    }
+
+    public IEnumerable<RouteInstruction> Routes { get; set; } = Enumerable.Empty<RouteInstruction>();
+}
