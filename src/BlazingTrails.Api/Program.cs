8ff0ada2f3558101b77e77296843df368c5cf003
Felipe Romero <feliperomero3@outlook.com>
2023-07-17 20:43:38 -0600

    Configuring the API to serve the Blazor WebAssembly application.

--- src/BlazingTrails.Api/Program.cs
+++ src/BlazingTrails.Api/Program.cs
@@ -1,7 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -11,12 +10,19 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.UseWebAssemblyDebugging();
 }
 
 app.UseHttpsRedirection();
 
+app.UseBlazorFrameworkFiles();
+app.UseStaticFiles();
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapFallbackToFile("index.html");
 
 app.Run();
